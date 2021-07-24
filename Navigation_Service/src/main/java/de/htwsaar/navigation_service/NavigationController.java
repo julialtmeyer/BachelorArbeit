ba756314b8e443a7e36fb41f4a113bcd83371657
Julian Altmeyer <julian.altmeyer95@gmail.com>
2021-07-24 03:08:23 +0200

    Navigation Service Nodes mit nur einem typen REST API

--- Navigation_Service/src/main/java/de/htwsaar/navigation_service/NavigationController.java
+++ Navigation_Service/src/main/java/de/htwsaar/navigation_service/NavigationController.java
@@ -1,12 +1,9 @@
 package de.htwsaar.navigation_service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import de.htwsaar.navigation_service.map.MapManager;
 import de.htwsaar.navigation_service.map.Node;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.BidirectionalDijkstraShortestPath;
-import org.jgrapht.alg.shortestpath.KShortestSimplePaths;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.DirectedWeightedMultigraph;
 import org.springframework.stereotype.Service;
@@ -26,11 +23,11 @@ public class NavigationController {
 
     public List<Node> pathFromPointAtoPointB(NavigationRequest navigationRequest){
 
-        Optional<Node> startNode = findNodeinGraphWithCoordinates(navigationRequest.getStart_x(), navigationRequest.getStart_y());
+        Optional<Node> startNode = findNodeInGraphWithCoordinates(navigationRequest.getStart_x(), navigationRequest.getStart_y());
         if(!startNode.isPresent()){
             System.out.println("no Node at start coordinates");
         }
-        Optional<Node> destNode = findNodeinGraphWithCoordinates(navigationRequest.getDest_x(), navigationRequest.getDest_y());
+        Optional<Node> destNode = findNodeInGraphWithCoordinates(navigationRequest.getDest_x(), navigationRequest.getDest_y());
         if(!destNode.isPresent()){
             System.out.println("no Node at destination coordinates");
         }
@@ -46,7 +43,7 @@ public class NavigationController {
         return  resultPath.getVertexList();
     }
 
-    protected Optional<Node> findNodeinGraphWithCoordinates(Integer x, Integer y){
+    protected Optional<Node> findNodeInGraphWithCoordinates(Integer x, Integer y){
 
         Optional<Node> node = manager.getGraph().vertexSet().stream()
                 .filter(n -> ((x <= n.getX()+configuration.getNodeToleranz()
