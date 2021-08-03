691b5a65f3de11847c701649cc68e0ddf391d6e8
Julian Altmeyer <julian.altmeyer95@gmail.com>
2021-08-03 15:50:06 +0200

    change how drive distance is calculated

--- Robot_Software/main.py
+++ Robot_Software/main.py
@@ -4,8 +4,13 @@ import brickpi3
 
 BP = brickpi3.BrickPi3()
 
-print(BP.get_motor_status(BP.PORT_A))
-BP.reset_all()
-print(BP.get_motor_status(BP.PORT_A))
+try:
 
-mqtt_client.connect_mqtt()
+    print(BP.get_motor_status(BP.PORT_A))
+    BP.reset_all()
+    print(BP.get_motor_status(BP.PORT_A))
+
+    mqtt_client.connect_mqtt()
+
+except KeyboardInterrupt:  # except the program gets interrupted by Ctrl+C on the keyboard.
+    BP.reset_all()
